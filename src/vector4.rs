aa2f5369d43068ea7c0336b8a22db96932d9fcc9
Temitope Alaga <temdog007@yahoo.com>
2020-10-29 15:07:38 -0700

    Finish vector

--- src/vector4.rs
+++ src/vector4.rs
@@ -48,18 +48,99 @@ impl Add for SimdVector4 {
     }
 }
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-fn x86_add(a: &Vector4<f32>, b: &Vector4<f32>) -> Vector4<f32> {
-    #[cfg(target_arch = "x86")]
-    use std::arch::x86::*;
-    #[cfg(target_arch = "x86_64")]
-    use std::arch::x86_64::*;
-
-    unsafe {
-        let a: __m128 = transmute(*a);
-        let b: __m128 = transmute(*b);
-        transmute(_mm_add_ps(a, b))
+impl AddAssign for SimdVector4 {
+    fn add_assign(&mut self, rhs: SimdVector4) {
+        *self = *self + rhs;
     }
+}
+
+impl Sub for SimdVector4 {
+    type Output = SimdVector4;
+    fn sub(self, rhs: SimdVector4) -> Self::Output {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        let vector = x86_sub(&self.vector, &rhs.vector);
+
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        let vector = self.vector - rhs.vector;
+
+        SimdVector4 { vector }
+    }
+}
+
+impl SubAssign for SimdVector4 {
+    fn sub_assign(&mut self, rhs: SimdVector4) {
+        *self = *self - rhs;
+    }
+}
+
+impl Mul for SimdVector4 {
+    type Output = SimdVector4;
+    fn mul(self, rhs: SimdVector4) -> Self::Output {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        let vector = x86_mul(&self.vector, &rhs.vector);
+
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        let vector = self.vector.component_mul(rhs.vector);
+
+        SimdVector4 { vector }
+    }
+}
+
+impl MulAssign for SimdVector4 {
+    fn mul_assign(&mut self, rhs: SimdVector4) {
+        *self = *self * rhs;
+    }
+}
+
+impl Div for SimdVector4 {
+    type Output = SimdVector4;
+    fn div(self, rhs: SimdVector4) -> Self::Output {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        let vector = x86_div(&self.vector, &rhs.vector);
+
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        let vector = self.vector.component_div(rhs.vector);
+
+        SimdVector4 { vector }
+    }
+}
+
+impl DivAssign for SimdVector4 {
+    fn div_assign(&mut self, rhs: SimdVector4) {
+        *self = *self / rhs;
+    }
+}
+
+#[cfg(target_arch = "x86")]
+use std::arch::x86::*;
+
+#[cfg(target_arch = "x86_64")]
+use std::arch::x86_64::*;
+
+fn x86_add(a: &Vector4<f32>, b: &Vector4<f32>) -> Vector4<f32> {
+    unsafe { x86_operation(|a, b| _mm_add_ps(a, b), a, b) }
+}
+
+fn x86_sub(a: &Vector4<f32>, b: &Vector4<f32>) -> Vector4<f32> {
+    unsafe { x86_operation(|a, b| _mm_sub_ps(a, b), a, b) }
+}
+
+fn x86_mul(a: &Vector4<f32>, b: &Vector4<f32>) -> Vector4<f32> {
+    unsafe { x86_operation(|a, b| _mm_mul_ps(a, b), a, b) }
+}
+
+fn x86_div(a: &Vector4<f32>, b: &Vector4<f32>) -> Vector4<f32> {
+    unsafe { x86_operation(|a, b| _mm_div_ps(a, b), a, b) }
+}
+
+unsafe fn x86_operation<F: Fn(__m128, __m128) -> __m128>(
+    f: F,
+    a: &Vector4<f32>,
+    b: &Vector4<f32>,
+) -> Vector4<f32> {
+    let a: __m128 = transmute(*a);
+    let b: __m128 = transmute(*b);
+    transmute(f(a, b))
 }
 
 #[cfg(test)]
@@ -77,7 +158,61 @@ mod tests {
 
         let a = SimdVector4::from(a);
         let b = SimdVector4::from(b);
+        let mut c = a;
+        c += b;
 
         assert!(result.relative_eq(&(a + b).into(), std::f32::EPSILON, std::f32::EPSILON));
+        assert!(result.relative_eq(&c.into(), std::f32::EPSILON, std::f32::EPSILON));
+    }
+
+    #[test]
+    fn sub_test() {
+        let mut rng = thread_rng();
+        let a = Vector4::<f32>::from_column_slice(&[rng.gen(), rng.gen(), rng.gen(), rng.gen()]);
+        let b = Vector4::<f32>::from_column_slice(&[rng.gen(), rng.gen(), rng.gen(), rng.gen()]);
+
+        let result = Vector4::from(a - b);
+
+        let a = SimdVector4::from(a);
+        let b = SimdVector4::from(b);
+        let mut c = a;
+        c -= b;
+
+        assert!(result.relative_eq(&(a - b).into(), std::f32::EPSILON, std::f32::EPSILON));
+        assert!(result.relative_eq(&c.into(), std::f32::EPSILON, std::f32::EPSILON));
+    }
+
+    #[test]
+    fn mul_test() {
+        let mut rng = thread_rng();
+        let a = Vector4::<f32>::from_column_slice(&[rng.gen(), rng.gen(), rng.gen(), rng.gen()]);
+        let b = Vector4::<f32>::from_column_slice(&[rng.gen(), rng.gen(), rng.gen(), rng.gen()]);
+
+        let result = Vector4::from(a.component_mul(&b));
+
+        let a = SimdVector4::from(a);
+        let b = SimdVector4::from(b);
+        let mut c = a;
+        c *= b;
+
+        assert!(result.relative_eq(&(a * b).into(), std::f32::EPSILON, std::f32::EPSILON));
+        assert!(result.relative_eq(&c.into(), std::f32::EPSILON, std::f32::EPSILON));
+    }
+
+    #[test]
+    fn div_test() {
+        let mut rng = thread_rng();
+        let a = Vector4::<f32>::from_column_slice(&[rng.gen(), rng.gen(), rng.gen(), rng.gen()]);
+        let b = Vector4::<f32>::from_column_slice(&[rng.gen(), rng.gen(), rng.gen(), rng.gen()]);
+
+        let result = Vector4::from(a.component_div(&b));
+
+        let a = SimdVector4::from(a);
+        let b = SimdVector4::from(b);
+        let mut c = a;
+        c /= b;
+
+        assert!(result.relative_eq(&(a / b).into(), std::f32::EPSILON, std::f32::EPSILON));
+        assert!(result.relative_eq(&c.into(), std::f32::EPSILON, std::f32::EPSILON));
     }
 }
